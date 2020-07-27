cf1bcd7882cecbcdf4d76955862a750e1666830c
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-07-26 22:01:47 -0500

    lots of work done. timer and scores still not done.

--- README.md
+++ README.md
@@ -1,42 +1,18 @@
 # 04 Web APIs: Code Quiz
 
-As you proceed in your career as a web developer, you will probably be asked to complete a coding assessment, which is typically a combination of multiple-choice questions and interactive challenges. Build a timed code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+This is a Quiz webpage (currently work in progress) using javascript tools
 
-## User Story
+## Instructions
+1. Press "start" button to start quiz
+    * countdown timer starts
+2. Answer quiz questions
+    * if answer is incorrect time is reduced by 5 seconds
+    * correct answers adds 5 to your score
+3. At the end of quiz Score and Seconds left is added to Final Score
+4. Enter name to save Score Results
 
-```
-AS A coding bootcamp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-```
 
-## Acceptance Criteria
+## Screen Shot
+![](assets/ScreenShot.png)
 
-```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score
-```
-
-The following animation demonstrates the application functionality:
-
-![code quiz](./Assets/04-web-apis-homework-demo.gif)
-
-### Review
-
-You are required to submit the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+## Link
--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,116 @@
+//START AND QUESTIONS SECTION
 var startButton = document.getElementById("startBtn")
+var nextButton = document.getElementById("nextBtn")
 var questContainerEl = document.getElementById("questionContainer")
 var questionEl = document.getElementById("question")
 var choicesEl = document.getElementById("choices")
-var currentQuestion
+var answerEl = document.getElementById("answerResult")
+var resultContainerEl = document.getElementById("resultContainter")
+var resultEl = document.getElementById("results")
+let currentQuestion, currentQuestionIndex
 
 startButton.addEventListener("click", start)
 
-var questions = [
+function start() {
+  console.log("start working")
+  startButton.classList.add("hide")
+  currentQuestion = questions
+  currentQuestionIndex = 0
+  questContainerEl.classList.remove("hide")
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  reSet()
+  showQuestion(currentQuestion[currentQuestionIndex])
+
+}
+
+function showQuestion(question) {
+  questionEl.innerText = question.question
+  question.choices.forEach(answer => {
+    const button = document.createElement("button")
+    button.innerText = answer.text
+    button.classList.add("btn")
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener("click", chooseFunction)
+    button.addEventListener("click", () => {
+      currentQuestionIndex++
+      setNextQuestion()
+    })
+    choicesEl.appendChild(button)
+
+  });
+}
+
+function reSet() {
+  while (choicesEl.firstChild) {
+    choicesEl.removeChild(choicesEl.firstChild)
+  }
+}
+
+function chooseFunction(e) {
+  var answerBtn = e.target
+  var correct = answerBtn.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(choicesEl.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (currentQuestion.length > currentQuestionIndex + 1) {
+    console.log("next question")
+  } else {
+    questContainerEl.classList.add("hide")
+    resultContainerEl.classList.remove("hide")
+    console.log("end of quiz")
+  }
+
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add("correct")
+  } else {
+    element.classList.add("wrong")
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct")
+  element.classList.remove("wrong")
+}
+
+const questions = [
   {
-    question: "Question 1?",
+    question: "Question 1??",
     choices: [
-      { text: "wrong", correct: false},
-      { text: "wrong", correct: false},
-      { text: "wrong", correct: false},
-      { text: "right", correct: true}
+      { text: "wrong", correct: false },
+      { text: "wrong", correct: false },
+      { text: "wrong", correct: false },
+      { text: "right", correct: true }
+    ]
+  },
+  {
+    question: "Question 2??",
+    choices: [
+      { text: "wrong", correct: false },
+      { text: "wrong", correct: false },
+      { text: "right", correct: true },
+      { text: "wrong", correct: false }
+    ]
+  }, {
+    question: "Question 3??",
+    choices: [
+      { text: "right", correct: true },
+      { text: "wrong", correct: false },
+      { text: "wrong", correct: false },
+      { text: "wrong", correct: false }
     ]
   }
 ]
 
-function start(){
-  console.log("start working");
-  startButton.classList.add("hide");
-  currentQuestion = 0;
-  questContainerEl.classList.remove("hide");
-}
-
-function Next(){
-  showQuestion(currentQuestion)
-
-}
-
-function showQuestion(question){
-  questionEl.innerText = question.question 
-}
-
-function choose(){
-
-}
+//TIMER SECTION//
+var minutesDisplay = document.querySelector("#minutes");
+var secondsDisplay = document.querySelector("#seconds");
